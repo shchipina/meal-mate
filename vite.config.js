ed363256412018d384706f142ce9eedf7509bfff
Olena Shchipina <140401215+shchipina@users.noreply.github.com>
2025-04-24 20:52:33 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    base: '/meal-mate/',
     react(),
     tailwindcss(),
   ],
