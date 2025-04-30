d0b785326975838d4faf02ae42ac5ee8aec5591a
Olena Shchipina <shipina.olena@gmail.com>
2025-04-30 16:47:22 +0300

    updated route

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,13 @@ import {
 } from '@tanstack/react-query'
 import { Provider } from 'react-redux'
 import { store } from './app/store.js'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './routes/routes.jsx'
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { App } from './App.jsx'
+import { Home } from "./pages/Home.jsx";
+import { Recipes } from "./pages/Recipes.jsx";
+import { Favorites } from "./pages/Favorites.jsx";
+import { RecipeDetails } from "./pages/RecipeDetails.jsx";
+import { WeeklyMenu } from "./pages/WeeklyMenu.jsx";
 
 const queryClient = new QueryClient();
 
@@ -16,7 +21,17 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
-        <RouterProvider router={router} />
+        <Router>
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route index element={<Home />} />
+              <Route path="recipes" element={<Recipes />} />
+              <Route path="favorites" element={<Favorites />} />
+              <Route path="recipes/details/:id" element={<RecipeDetails />} />
+              <Route path="weekly-menu" element={<WeeklyMenu />} />
+            </Route>
+          </Routes>
+        </Router>
       </Provider>
     </QueryClientProvider>
   </StrictMode>,
